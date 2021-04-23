59530054eb0a06749630d9fe88cbbac5c5ce874a
Andrew Broomfield <82602104+AJBroomfield@users.noreply.github.com>
2021-04-23 14:08:07 +0100

    Update near.py
    removed unnecessary code

--- near.py
+++ near.py
@@ -14,12 +14,4 @@ for i in range(len(stringa)):
 print(string1==list(stringb))
 
 
-# for j in range(0):
-#     for i in range(len(stringa)):
-#         string1=list(stringa)
-#         del(string1[i])
-#         #print(f'{i} {string1} \n  {list(stringb)}')
-#         if string1==list(stringb):
-#             print('TRUE')
-#             break
-# print('FALSE')
+
