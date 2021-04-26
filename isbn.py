bf2523c0e0400208faa0ed8df20f26cc36e0a495
Andrew Broomfield <abroomfield@qa.com>
2021-04-26 13:08:11 +0000

    Changed isbn.py to a function

--- isbn.py
+++ isbn.py
@@ -1,22 +1,26 @@
-isbn = '978-0-306-40615-7'
-#isbn = '978-0-14-101111-0'
-#isbn = '978-0-575-08625-8'
-#isbn ='9781861972712'
-short = isbn.replace('-','',-1)
-total = 0
+def isbncheck(code):
+    short = code.replace('-','',-1)
+    total = 0
 
-for i in range(len(short)-1):
-    if i % 2 == 0:
-        total += int(short[i])
+    for i in range(len(short)-1):
+        if i % 2 == 0:
+            total += int(short[i])
+        else:
+            total += 3*int(short[i])
+
+    #print(int(short[-1]), 10-(total%10))
+    #print(type((short[-1])), type(10-(total%10)))
+
+    if int(short[-1]) == 10-(total%10):
+        check = 'Code valid!'
+    elif int(short[-1]) == 0 and 10-(total%10) == 10:
+        check = 'Code valid!'
     else:
-        total += 3*int(short[i])
+        check = 'Not a book nerd'
+    print(check)
 
-#print(int(short[-1]), 10-(total%10))
-#print(type((short[-1])), type(10-(total%10)))
 
-if int(short[-1]) == 10-(total%10):
-    print("code valid")
-elif int(short[-1]) == 0 and 10-(total%10) == 10:
-    print("code valid")
-else:
-    print("Not a book, nerd")
+isbncheck('978030640615-7')
+isbncheck('978-0-14-101111-0')
+isbncheck('978-0-575-08625-8')
+isbncheck('978186197274712')
