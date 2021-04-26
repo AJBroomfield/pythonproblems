dd4b44b983bdb48ab8922de48e2fa6c5dd97c253
Andrew Broomfield <abroomfield@qa.com>
2021-04-26 11:28:21 +0000

    QA community updated file

--- grading.py
+++ grading.py
@@ -12,14 +12,11 @@ def it_average(name, homework, assessment, exam):
         grade = 'Fail'
     return(f'{name} your score was {round(overall,2)}% which is a {grade}')
 
-# name = input("Type the students name: ")
-# homework = int(input("What did they score on their homework? "))
-# assessment = int(input("What did they score on their assessment? "))
-# exam = int(input("What did they score on their exam? "))
+name = input("Type the students name: ")
+homework = int(input("What did they score on their homework? "))
+assessment = int(input("What did they score on their assessment? "))
+exam = int(input("What did they score on their exam? "))
 
-name = 'Brian'
-homework = 22
-assessment = 38
-exam = 72
+
 
 print(it_average(name, homework, assessment, exam))
